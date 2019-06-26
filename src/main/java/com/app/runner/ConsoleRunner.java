df63031a00d6273304f1fa68bbef82a1c6019f5a
new <new@DESKTOP-TCT062V>
2019-06-26 15:29:43 +0530

    ashok

---
+++ src/main/java/com/app/runner/ConsoleRunner.java
@@ -0,0 +1,24 @@
+package com.app.runner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.app.document.Address;
+import com.app.document.Employee;
+import com.app.repo.EmployeeRepository;
+@Component
+public class ConsoleRunner implements CommandLineRunner {
+
+	@Autowired
+	private EmployeeRepository repo;
+	@Override
+	public void run(String... args) throws Exception {
+
+		repo.deleteAll();
+		
+		repo.save(new Employee(10, "AAA",(new Address("2-31","Hyd"))));
+		System.out.println("done");
+	}
+
+}
